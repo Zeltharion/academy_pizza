b7a7a1f6447a978c73407d67f9312cd39a05364a
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-09-26 20:07:25 +0300

    added custom theme with shadcn/ui added components for top side to main page added nunito font to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+import { Container, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
-    <h1>
-      home
-    </h1>
+    <>
+      <Container className="mt-10">
+        <Title
+          text="Все пиццы"
+          size="lg"
+          className="font-extrabold" />
+      </Container>
+
+      <TopBar />
+
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+
+          <div className="w-[250px]">
+            Filters
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              Product list
+            </div>
+          </div>
+
+        </div>
+      </Container>
+
+    </>
   );
 }
