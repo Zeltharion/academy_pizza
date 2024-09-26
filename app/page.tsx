972bfb0d63515fffb24ac8766aceda0640fec54e
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-09-27 00:17:11 +0300

    app: added ProductsGroupList render

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,37 @@
-import { Container, Title, TopBar } from "@/components/shared";
+import { Container, Filters, ProductsGroupList, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
+  const pizzaItems = [
+    {
+      id: 1,
+      name: "Пепперони",
+      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+      price: 500,
+      items: [{price: 500}],
+    },
+    {
+      id: 2,
+      name: "Маргарита",
+      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+      price: 500,
+      items: [{price: 500}],
+    },
+    {
+      id: 3,
+      name: "Мексиканская",
+      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+      price: 500,
+      items: [{price: 500}],
+    },
+    {
+      id: 4,
+      name: "Гавайская",
+      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+      price: 500,
+      items: [{price: 500}],
+    }
+  ];
+
   return (
     <>
       <Container className="mt-10">
@@ -12,16 +43,20 @@ export default function Home() {
 
       <TopBar />
 
-      <Container className="pb-14">
-        <div className="flex gap-[60px]">
+      <Container className="my-10">
+        <div className="flex gap-[80px]">
 
           <div className="w-[250px]">
-            Filters
+            <Filters />
           </div>
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              Product list
+              <ProductsGroupList 
+                title="Пиццы"
+                categoryId={0}
+                items={pizzaItems}
+              />
             </div>
           </div>
 
