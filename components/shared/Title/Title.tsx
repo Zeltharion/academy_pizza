9275b22af39228a246fcb3d237498c3b3d5c5425
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-09-27 00:05:44 +0300

    components shared: reogranized structure of component folder (types, scss, tsx component)

--- components/shared/Title/Title.tsx
+++ components/shared/Title/Title.tsx
@@ -1,15 +1,8 @@
 import clsx from 'clsx';
 import React from 'react';
+import { ITitle } from './Title.types';
 
-type TitleSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-
-interface Props {
-	size?: TitleSize;
-	className?: string;
-	text: string;
-}
-
-export const Title: React.FC<Props> = ({ text, size = 'sm', className }) => {
+export const Title: React.FC<ITitle> = ({ text, size = 'sm', className }) => {
 	const mapTagBySize = {
 		xs: 'h5',
 		sm: 'h4',
@@ -20,12 +13,12 @@ export const Title: React.FC<Props> = ({ text, size = 'sm', className }) => {
 	} as const;
 
 	const mapClassNameBySize = {
-		xs: 'text-[16px] ys-text',
-		sm: 'text-[22px] ys-text',
-		md: 'text-[26px] ys-text',
-		lg: 'text-[32px] ys-display',
-		xl: 'text-[40px] ys-display',
-		'2xl': 'text-[48px] ys-display',
+		xs: 'text-[16px]',
+		sm: 'text-[22px]',
+		md: 'text-[26px]',
+		lg: 'text-[32px]',
+		xl: 'text-[40px]',
+		'2xl': 'text-[48px]',
 	} as const;
 
 	return React.createElement(
