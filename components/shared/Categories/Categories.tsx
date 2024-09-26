56af642ae340332ebcdd894003fba75341e90980
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-09-27 00:51:49 +0300

    components shared: added logic to navigate between categories of products

--- components/shared/Categories/Categories.tsx
+++ components/shared/Categories/Categories.tsx
@@ -1,24 +1,34 @@
+'use client'
+
 import { cn } from "@/lib/utils"
 import s from './Categories.module.scss'
+import { useCategoryStore } from "@/store/category";
 
 interface ICategories {
 	className?: string;
 }
 
-const cats = ['Пиццы', 'Комбо', 'Закуски', 'Коктейли', 'Кофе', 'Напитки', 'Десерты']
-const activeIndex = 0;
+const cats = [
+	{ id: 1, name: 'Пиццы' },
+	{ id: 2, name: 'Комбо' },
+	{ id: 3, name: 'Закуски' },
+	{ id: 4, name: 'Коктейли' },
+	{ id: 5, name: 'Кофе' },
+	{ id: 6, name: 'Напитки' },
+	{ id: 7, name: 'Десерты' },
+]
 
 export const Categories: React.FC<ICategories> = ({ className }) => {
+	const categoryActiveId = useCategoryStore((state) => state.activeId)
 
 	return (
 		<div className={cn(s.categories, className)}>
-			{cats.map((cat, index) => (
-				<a className={cn(s.categoryItem,
-					activeIndex === index && s.active
-				)}
-					key={index}>
+			{cats.map((cats, index) => (
+				<a className={cn(s.categoryItem, categoryActiveId === cats.id && s.active)}
+					key={index}
+					href={`/#${cats.name}`}>
 					<button>
-						{cat}
+						{cats.name}
 					</button>
 				</a>
 			))}
