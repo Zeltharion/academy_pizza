3b55dcd3d88ff94fcf6fa3a1673f0e660833a68d
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-01 02:21:52 +0300

    components shared: added categories props for TopBar component

--- components/shared/TopBar/TopBar.tsx
+++ components/shared/TopBar/TopBar.tsx
@@ -1,16 +1,18 @@
 import { cn } from "@/lib/utils"
 import s from './TopBar.module.scss'
 import { Categories, Container, SortPopup } from "@/components/shared"
+import { Category } from "@prisma/client"
 
 interface ITopBar {
+	categories: Category[]
 	className?: string
 }
 
-export const TopBar: React.FC<ITopBar> = ({ className }) => {
+export const TopBar: React.FC<ITopBar> = ({ categories, className }) => {
 	return (
 		<div className={cn(s.topBar, className)}>
 			<Container className={s.container}>
-				<Categories />
+				<Categories items={categories}/>
 				<SortPopup />
 			</Container>
 		</div>
