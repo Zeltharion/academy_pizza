95e80de451ae5a52e5daecccf29a7cab2aaec03c
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-04 03:02:58 +0300

    constants: created pizzaTypes

--- shared/constants/pizza.ts
+++ shared/constants/pizza.ts
@@ -10,12 +10,16 @@ const mapPizzaType = {
 } as const;
 
 const pizzaSizes = Object.entries(mapPizzaSize).map(([value, name]) => ({
-	id: Number(name),
 	name,
 	value,
 }))
  
+const pizzaTypes = Object.entries(mapPizzaType).map(([value, name]) => ({
+	name,
+	value,
+}))
+
 export type PizzaSize = keyof typeof mapPizzaSize;
 export type PizzaType = keyof typeof mapPizzaType;
 
-export { mapPizzaSize, mapPizzaType, pizzaSizes }
+export { mapPizzaSize, mapPizzaType, pizzaSizes, pizzaTypes }
