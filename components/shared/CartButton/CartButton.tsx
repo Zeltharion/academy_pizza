088e95dc5201dcfbd83165bf02bbf737e16669d1
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-05 02:07:44 +0300

    components shared: CartButton connected with Cart store

--- components/shared/CartButton/CartButton.tsx
+++ components/shared/CartButton/CartButton.tsx
@@ -1,24 +1,31 @@
+'use client'
+
 import { Button } from "@/components/ui";
 import { cn } from "@/shared/lib";
 import { ShoppingCart, ArrowRight } from "lucide-react";
 import { CartDrawer } from "@/components/shared";
+import { useCartStore } from "@/shared/store";
 
 interface ICartButton {
 	className?: string;
 }
 
 export const CartButton: React.FC<ICartButton> = ({ className }) => {
+	const { totalAmount, items, loading } = useCartStore()
+
 	return (
 		<CartDrawer>
-			<Button className={cn('group relative', className)}>
-				<b>520 ₽</b>
+			<Button
+				className={cn('group relative', { 'w-[105px]': loading }, className)}
+				loading={loading}>
+				<b>{totalAmount} ₽</b>
 				<span className='h-full w-[1px] bg-white/30 mx-3' />
 				<div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
 					<ShoppingCart
 						className='relative'
 						strokeWidth={2}
 						size={16} />
-					<b>3</b>
+					<b>{items.length}</b>
 				</div>
 				<ArrowRight
 					className='right-5 absolute transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0'
