dac4c786d79db2fd0987d3a7e2221bf6708936b0
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-04 03:44:05 +0300

    components shared: updated exports

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -18,5 +18,7 @@ export * from './ProductVariantSelector/ProductVariantSelector'
 export * from './ProductForm/ProductForm'
 export * from './PizzaForm/PizzaForm'
 export * from './IngredientCard/IngredientCard'
+export * from './CartButton/CartButton'
+export * from './CartDrawer/CartDrawer'
 
 export * from './modals'
