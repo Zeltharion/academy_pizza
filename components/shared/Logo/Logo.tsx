a8d78fdb76c67628c2c1aa52fbca2059bfacfcf1
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-06 05:01:22 +0300

    components shared: created Logo

---
+++ components/shared/Logo/Logo.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import s from './Logo.module.scss';
+
+export const Logo: React.FC = () => {
+	return (
+		<Link href='/' className={s.logo}>
+			<Image
+				src='/logo.png'
+				alt='logo'
+				width={35}
+				height={35}
+				priority />
+			<div className={s.logo__text}>
+				<h1>Next Pizza</h1>
+				<p>вкусней уже некуда</p>
+			</div>
+		</Link>
+	);
+}
