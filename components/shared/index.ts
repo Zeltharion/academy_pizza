87e82ed649ed52bea5d5b65386b6da7ce8dc09af
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-06 05:12:32 +0300

    components shared: updated exports

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -3,10 +3,10 @@ export * from './Title/Title'
 export * from './Header/Header'
 export * from './TopBar/TopBar'
 export * from './SearchInput/SearchInput'
+export * from './Logo/Logo'
 
 export * from './Categories/Categories'
 export * from './SortPopup/SortPopup'
-
 export * from './Filters/Filters'
 export * from './FilterCheckbox/FilterCheckbox'
 export * from './FiltersCheckboxGroup/FiltersCheckboxGroup'
@@ -26,5 +26,11 @@ export * from './CartDrawerItem/CartDrawerItem'
 export * from './CartItemDetails'
 export * from './CountButtonContainer/CountButtonContainer'
 export * from './CountIconButton/CountIconButton'
+
+export * from './WhiteBlock/WhiteBlock'
+export * from './CheckoutDetails/CheckoutDetails'
+export * from './CheckoutItem/CheckoutItem'
+export * from './CheckoutItemSkeletom/CheckoutItemSkeleton'
+export * from './СheckoutSidebar/СheckoutSidebar'
 
 export * from './modals'
