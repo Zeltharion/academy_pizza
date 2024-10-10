dc4010b351b5b73f689cde3a3c81d6bbc9e83a28
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-11 02:07:39 +0300

    api: created auth/me endpoint

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/prisma/prismaClient";
+import { getUserSession } from "@/shared/lib/getUserSession";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+	try {
+		const user = await getUserSession();
+		if (!user) {
+			return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+		}
+
+		const data = await prisma.user.findFirst({
+			where: {
+				id: Number(user.id),
+			},
+			select: {
+				fullName: true,
+				email: true,
+				password: false,
+			},
+		})
+
+		return NextResponse.json(data);
+	} catch (error) {
+		console.error("[API_AUTH_ME]: ", error);
+		return NextResponse.json({ message: "[API_AUTH_ME] Server error" }, { status: 500 });
+	}
+}
