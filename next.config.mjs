a9b0df547bdcbada4356e6be796a700646fdeb3f
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-11 01:34:47 +0300

    next config: toggled reactStrictMode to false

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: false,
 	env: {
 		DADATA_API_TOKEN: process.env.DADATA_API_TOKEN,
+		RESEND_API_TOKEN: process.env.RESEND_API_TOKEN,
+		YOOKASSA_API_TOKEN: process.env.YOOKASSA_API_TOKEN,
+		YOOKASSA_RETURN_URL: process.env.YOOKASSA_RETURN_URL,
+		YOOKASSA_SHOP_ID: process.env.YOOKASSA_SHOP_ID,
 	},
 };
 
