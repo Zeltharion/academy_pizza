757803261237d2f86f9babc95d5e102818fa3b6b
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-11 01:30:57 +0300

    lib: added JSDoc getCartDetails

--- shared/lib/getCartDetails.ts
+++ shared/lib/getCartDetails.ts
@@ -18,7 +18,23 @@ interface IReturnData {
 	totalAmount: number;
 }
 
-
+/**
+ * Given a CartDTO, returns an object with 'totalAmount' and 'items' properties.
+ * 'totalAmount' is the total amount of the cart, and 'items' is an array of
+ * objects with the following properties:
+ *   - id: the id of the cart item
+ *   - quantity: the quantity of the item in the cart
+ *   - name: the name of the item
+ *   - imageUrl: the URL of the image of the item
+ *   - price: the total price of the item (item price * quantity)
+ *   - pizzaSize: the size of the pizza, if applicable
+ *   - pizzaType: the type of pizza, if applicable
+ *   - disabled: a boolean indicating whether the item is disabled (e.g. if it's out of stock)
+ *   - ingredients: an array of objects with the name and price of each ingredient
+ * @param data - The CartDTO to convert to a CartStateItem[]
+ * @example getCartDetails(data)
+ * @returns The converted CartStateItem[]
+ */
 export const getCartDetails = (data: CartDTO): IReturnData => {
 	const items = data.items.map((item) => ({
 		id: item.id,
