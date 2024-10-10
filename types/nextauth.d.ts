ee2c35b2c7b2d161d2034b471e63cb32cf32b238
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-11 01:56:57 +0300

    types: created types for nextauth

---
+++ types/nextauth.d.ts
@@ -0,0 +1,26 @@
+import { DefaultSession, DefaultUser } from 'next-auth';
+import { JWT, DefaultJWT } from 'next-auth/jwt';
+import type { UserRole } from '@prisma/client';
+
+declare module 'next-auth' {
+	interface Session {
+		user: {
+			id: string;
+			role: UserRole;
+			name: string;
+			image: string;
+		};
+	}
+
+	interface User extends DefaultUser {
+		id: number;
+		role: UserRole;
+	}
+}
+
+declare module 'next-auth/jwt' {
+	interface JWT extends DefaultJWT {
+		id: string;
+		role: UserRole;
+	}
+}
