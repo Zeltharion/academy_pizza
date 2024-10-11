aa9307758ae744ba69b3bb429794b2ba5fbc94a1
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-11 22:58:40 +0300

    lib: some fixes in formatIngredientsArrayToText

--- shared/lib/formatIngredientsArrayToText.ts
+++ shared/lib/formatIngredientsArrayToText.ts
@@ -1,6 +1,7 @@
 import { Ingredient } from "@prisma/client"
 
 export const formatIngredientsArrayToText = (ingredient: Ingredient[]) => {
+	if (!ingredient) return '';
 	return ingredient.map((ingredient, index) =>
 		index === 0 ? ingredient.name : ingredient.name.charAt(0).toLowerCase() + ingredient.name.slice(1)
 	).join(', ')
