33fe0ad1b7a0d5a1a193e867f9de94608dbaf1c5
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-11 21:48:23 +0300

    pages: added forbidden conditions on admin page

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,4 +1,28 @@
-export default function Admin() {
+import { prisma } from "@/prisma/prismaClient";
+import { getUserSession } from "@/shared/lib/getUserSession";
+import { UserRole } from "@prisma/client";
+import { redirect } from "next/navigation";
+
+export default async function Admin() {
+	const session = await getUserSession()
+	if (!session) {
+		return redirect('/forbidden')
+	}
+
+	const user = await prisma.user.findFirst({
+		where: {
+			id: Number(session.id),
+		}
+	})
+
+	if (!user) {
+		return redirect('/forbidden')
+	}
+
+	if (user.role !== UserRole.ADMIN) {
+		return redirect('/forbidden')
+	}
+
 	return (
 		<div>Admin Dashboard</div>
 	);
