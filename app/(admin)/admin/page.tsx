e37104113789c116ef17a076e50a5d762caf1829
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-10-13 04:53:10 +0300

    pages: edited admin home page

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,12 +1,14 @@
 import { prisma } from "@/prisma/prismaClient";
-import { getUserSession } from "@/shared/lib/getUserSession";
 import { UserRole } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { getUserSession } from "@/shared/lib/getUserSession";
+import urls from "@/shared/config/urls";
+import s from './adminPage.module.scss'
 
 export default async function Admin() {
 	const session = await getUserSession()
 	if (!session) {
-		return redirect('/forbidden')
+		return redirect(urls.forbidden)
 	}
 
 	const user = await prisma.user.findFirst({
@@ -16,14 +18,19 @@ export default async function Admin() {
 	})
 
 	if (!user) {
-		return redirect('/forbidden')
+		return redirect(urls.forbidden)
 	}
 
 	if (user.role !== UserRole.ADMIN) {
-		return redirect('/forbidden')
+		return redirect(urls.forbidden)
 	}
 
 	return (
-		<div>Admin Dashboard</div>
+		<div className={s.adminPage__home}>
+			<p className={s.adminPage__home__title}>
+				<span className={s.adminPage__home__title__name}>{user.fullName}</span>, добро пожаловать в админ панель!
+			</p>
+			<p className={s.adminPage__home__text}>Для продолжения работы выберите пункт в меню слева.</p>
+		</div>
 	);
 }
